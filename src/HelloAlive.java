588328127dcf37c149e077b034f467b21044dd49
Thouze-JavaAdv <th.tensorflow@gmail.com>
2019-09-04 14:28:56 -0400

    my ReadMe file

---
+++ src/HelloAlive.java
@@ -0,0 +1,7 @@
+public class HelloAlive {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Today is September 4, 2019. I am definitely alive!");
+    }
+
+}
